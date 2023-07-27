9c314dc998bebd917e71e5d9aca5accee60e5e40
Tonmoy Mondal <dev.tonmoy@proton.me>
2023-07-27 13:59:36 +0530

    01

---
+++ 01_Introduction/01_Welcome to Java.java
@@ -0,0 +1,13 @@
+/*
+** Author: TONMOY M.
+** URI   : https://qinetique.github.io/
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
