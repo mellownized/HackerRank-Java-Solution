b5f94dfcd080b96cbb95b56d7fdfcd43921e415e
Tonmoy Mondal <dev.tonmoy@proton.me>
2023-07-27 14:30:13 +0530

    Create 07_Java Loops II.java

---
+++ 01_Introduction/07_Java Loops II.java
@@ -0,0 +1,27 @@
+/*
+** Author: TONMOY M.
+** URI   : https://qinetique.github.io/
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int solution = a;
+            for(int j = 0; j < n; j++){
+                solution += (int) (Math.pow(2,j) * b);
+                System.out.print(solution + " ");
+            }
+            System.out.print("\n");
+        }
+        in.close();
+    }
+}
