b8c29bcf00036ca380d0501ed98175ef3b078ace
Tonmoy Mondal <dev.tonmoy@proton.me>
2023-07-27 14:06:14 +0530

    Create 02_ Java Stdin and Stdout I.java

---
+++ 01_Introduction/02_ Java Stdin and Stdout I.java
@@ -0,0 +1,25 @@
+/*
+** Author: TONMOY M.
+** URI   : https://qinetique.github.io/
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        // Complete this line
+        int b = scanner.nextInt();
+        // Complete this line
+        int c = scanner.nextInt();
+        
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
