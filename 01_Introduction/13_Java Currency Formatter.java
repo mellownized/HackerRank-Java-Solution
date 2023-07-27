4188eb747eb7e54224e6381a8b3564779fa37051
Tonmoy Mondal <dev.tonmoy@proton.me>
2023-07-27 16:40:14 +0530

    introduction - java

---
+++ 01_Introduction/13_Java Currency Formatter.java
@@ -0,0 +1,31 @@
+/*
+** Author: TONMOY M.
+** URI   : https://qinetique.github.io/
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
